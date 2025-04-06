93bbfc2b3c28692153c0948cbe970ecb7a8184fb
Astrail5258 <120449933+Astrail5258@users.noreply.github.com>
2025-04-06 12:31:19 +0300

    Create cB.lua

---
+++ cB.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrail5258/Skibidi/refs/heads/main/bH.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrail5258/Skibidi/refs/heads/main/gR.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Astrail5258/Skibidi/refs/heads/main/hR.lua"))()
